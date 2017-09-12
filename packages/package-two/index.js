9685108308867db3a37478a0aeed54d28f8956aa
Chris Slater <chris.slater@gamesys.co.uk>
2017-09-12 17:37:13 +0100

    Do stuff

--- packages/package-two/index.js
+++ packages/package-two/index.js
@@ -1,8 +1,9 @@
+const add = require('package-three').add
 const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-	res.send('Hello World!')
+	res.send(`Hello World! ${add(1 + 4)}`)
 })
 
 app.listen(3000, function () {
---
+++ start-dev.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker-compose up -f dc.dev.yml --build "$@"
