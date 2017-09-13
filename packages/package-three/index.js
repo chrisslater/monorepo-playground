68e2f87567febb73f8b3cc1b600de4ff94e61859
Christopher Slater <chris@snapper.fish>
2017-09-13 07:25:37 +0100

    things

--- packages/package-three/index.js
+++ packages/package-three/index.js
@@ -1,1 +1,2 @@
-export const add = (a, b) => a + b
+module.exports.add = (a, b) => a + b
+module.exports.subtract = (a, b) => a - b
---
+++ packages/package-two/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ln -s /tmp/node_modules /code
+yarn debug
