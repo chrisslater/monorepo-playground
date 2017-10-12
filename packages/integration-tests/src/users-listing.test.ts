f5bfdc27a1a62d6c6d4b32244cf295e6cd150e4d
Christopher Slater <chris@snapper.fish>
2017-10-12 12:36:04 +0300

    Rename integration test

--- packages/integration-tests/src/users-listing.test.ts
+++ packages/integration-tests/src/users-listing.test.ts
@@ -1,10 +1,10 @@
 import { Selector } from 'testcafe' // first import testcafe selectors
 
-fixture('Getting Started') // declare the fixture
+fixture('Users listing') // declare the fixture
 	.page(`http://localhost:3000`) // specify the start page
 
 // then create a test and place your code there
-test('My first test', async (t) => {
+test('Chris should appear first in users list', async (t) => {
 	await t
 		// Use the assertion to check if the actual header text is equal to the expected one
 		.expect(Selector('[data-test="users"] div div').innerText).eql('Chris')
